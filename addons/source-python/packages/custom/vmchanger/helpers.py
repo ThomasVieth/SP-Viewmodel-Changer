ba171c055f990d0611e6f509bfb6f1138433f4a2
Stephen Toon <satoon101@gmail.com>
2017-03-05 00:37:54 -0500

    Updated to not have to use get_property_int directly.
    All weapons should have m_Owner as the owner_handle, since CBaseCombatWeapon should be searched prior to CBaseEntity for dynamic attributes.

--- addons/source-python/packages/custom/vmchanger/helpers.py
+++ addons/source-python/packages/custom/vmchanger/helpers.py
@@ -11,6 +11,5 @@ __all__ = ('get_predicted_view_model', )
 def get_predicted_view_model(player):
 	'Retrieves the current view model predictor representative of the player.'
 	for entity in EntityIter('predicted_viewmodel'):
-		if entity.get_property_int('m_hOwner') != player.inthandle:
-			continue
-		return entity
+		if entity.owner_handle == player.inthandle:
+			return entity
